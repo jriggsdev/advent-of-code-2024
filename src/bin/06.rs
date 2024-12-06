1bb380c5463bdc70a5403af431f9476998a67a47
josh <jriggsdev@gmail.com>
2024-12-06 12:39:57 -0500

    Day 6 part 2 Its not the cleanest and its certainly not the fastest but it gets the right answer

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,13 +1,13 @@
 advent_of_code::solution!(6);
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Space{
     Unvisited,
-    Visited,
+    Visited(Direction),
     Obstacle,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Direction {
     North,
     East,
@@ -19,9 +19,10 @@ enum Direction {
 enum StepError {
     Blocked,
     OutOfMap,
+    CaughtInLoop,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Map {
     spaces: Vec<Vec<Space>>,
 }
@@ -58,9 +59,13 @@ impl Guard {
             return Err(StepError::Blocked);
         }
 
+        if map.spaces[new_x as usize][new_y as usize] == Space::Visited(self.direction.clone()) {
+            return Err(StepError::CaughtInLoop)
+        }
+
         self.position.0 = new_x as u32;
         self.position.1 = new_y as u32;
-        map.spaces[self.position.0 as usize][self.position.1 as usize] = Space::Visited;
+        map.spaces[self.position.0 as usize][self.position.1 as usize] = Space::Visited(self.direction.clone());
         Ok(())
     }
 
@@ -95,7 +100,7 @@ fn parse_input(input: &str) -> (Map, Guard) {
                 '^' => {
                     guard.position.0 = map.spaces.len() as u32;
                     guard.position.1 = current_map_line.len() as u32;
-                    current_map_line.push(Space::Visited);
+                    current_map_line.push(Space::Visited(Direction::North));
                 },
                 _ => {
                     panic!("Unexpected space in map.")
@@ -122,20 +127,79 @@ pub fn part_one(input: &str) -> Option<u32> {
             },
             Err(StepError::OutOfMap) => {
                 break;
+            },
+            Err(StepError::CaughtInLoop) => {
+                panic!("Guard caught in loop.");
             }
         }
     }
 
     let spaces_visited = map.spaces.iter()
         .flat_map(|line| line)
-        .filter(|space| **space == Space::Visited)
+        .filter(|space| match **space { Space::Visited(_) => true, _ => false })
         .count() as u32;
 
     Some(spaces_visited)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (map, mut guard) = parse_input(input);
+
+    let mut initial_run_map = map.clone();
+    let guard_starting_point = guard.position;
+    loop {
+        let step_result = guard.step(&mut initial_run_map);
+        match step_result {
+            Ok(_) => {
+            },
+            Err(StepError::Blocked) => {
+                guard.turn();
+            },
+            Err(StepError::OutOfMap) => {
+                break;
+            },
+            Err(StepError::CaughtInLoop) => {
+                panic!("Guard caught in loop.");
+            }
+        }
+    }
+
+    let mut total = 0;
+    for i in 0..initial_run_map.spaces.len() {
+        for j in 0..initial_run_map.spaces[i].len() {
+            match &initial_run_map.spaces[i][j] {
+                Space::Visited(_) => {
+                    if guard_starting_point != (i as u32, j as u32) {
+                        let mut new_map = map.clone();
+                        new_map.spaces[i][j] = Space::Obstacle;
+                        guard.position = guard_starting_point;
+                        guard.direction = Direction::North;
+
+                        loop {
+                            let step_result = guard.step(&mut new_map);
+                            match step_result {
+                                Ok(_) => {
+                                },
+                                Err(StepError::Blocked) => {
+                                    guard.turn();
+                                },
+                                Err(StepError::OutOfMap) => {
+                                    break;
+                                },
+                                Err(StepError::CaughtInLoop) => {
+                                    total += 1;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                },
+                _ => {}
+            }
+        }
+    }
+
+    Some(total)
 }
 
 #[cfg(test)]
@@ -151,6 +215,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }

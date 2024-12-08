c841afe422ed98a1cdc987130894fd8e6ed28349
josh <jriggsdev@gmail.com>
2024-12-07 21:53:26 -0500

    day 7 part 2 Its slow but it works

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -78,6 +78,22 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(result)
 }
 
+enum Operator {
+    Multiply,
+    Add,
+    Concat,
+}
+
+impl Operator {
+    fn apply(&self, a: u64, b: u64) -> u64 {
+        match self {
+            Operator::Multiply => a * b,
+            Operator::Add => a + b,
+            Operator::Concat => format!("{a}{b}").parse::<u64>().unwrap(),
+        }
+    }
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
     let mut result = 0;
     for line in input.lines() {
@@ -92,6 +108,39 @@ pub fn part_two(input: &str) -> Option<u64> {
             .filter(|str| !str.is_empty())
             .map(|str| str.trim().parse::<u64>().unwrap())
             .collect();
+        let num_operators = numbers.len() - 1;
+        let num_operator_combinations = 3_u32.pow(num_operators as u32);
+
+        for i in 0..num_operator_combinations {
+            let mut operators: Vec<Operator> = vec!();
+            let mut num = i;
+            while num > 0 {
+                let remainder = num % 3;
+                match remainder {
+                    0 => operators.push(Operator::Add),
+                    1 => operators.push(Operator::Multiply),
+                    2 => operators.push(Operator::Concat),
+                    _ => panic!("Something unexpected happened. Cosmic rays flipped a bit maybe?")
+                }
+                num /= 3;
+            }
+            while operators.len() < num_operators {
+                operators.push(Operator::Add);
+            }
+
+            let mut running_value = numbers[0];
+            for j in 0..num_operators {
+                running_value = operators[j].apply(running_value, numbers[j + 1]);
+                if running_value > test_value {
+                    break;
+                }
+            }
+
+            if running_value == test_value {
+                result += test_value;
+                break;
+            }
+        }
     }
 
     Some(result)

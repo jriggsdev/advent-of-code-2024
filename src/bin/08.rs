8f5dd18180131810101b8904820f0357a75365d1
josh <jriggsdev@gmail.com>
2024-12-08 20:19:07 -0500

    Day 8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,8 +1,9 @@
+use std::cmp::min;
 use std::collections::{HashMap, HashSet};
 
 advent_of_code::solution!(8);
 
-#[derive(Eq, PartialEq, Hash)]
+#[derive(Eq, PartialEq, Hash, Clone, Copy)]
 struct Coordinate {
     x: i32,
     y: i32,
@@ -59,7 +60,83 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut antennas: HashMap<char, Vec<Coordinate>> = HashMap::new();
+    let mut map_height = 0;
+    let mut map_width = 0;
+    for (i, line) in input.lines().enumerate() {
+        map_height += 1;
+        if line.is_empty() {
+            break;
+        }
+        for (j, char) in line.chars().enumerate() {
+            if i == 0 {
+                map_width += 1;
+            }
+            if char == '.' {
+                continue;
+            }
+            if let Some(coordinates) = antennas.get_mut(&char) {
+                coordinates.push(Coordinate { x: i as i32, y: j as i32 });
+            } else {
+                antennas.insert(char, vec![Coordinate { x: i as i32, y: j as i32 }]);
+            }
+        }
+    }
+
+    let mut antinode_locations: HashSet<Coordinate> = HashSet::new();
+    for (_, coordinates) in antennas.iter() {
+        for i in 0..coordinates.len() {
+            for j in i + 1..coordinates.len() {
+                let a = &coordinates[i];
+                let b = &coordinates[j];
+                let mut x_diff = a.x - b.x;
+                let mut y_diff = a.y - b.y;
+
+                for i in 2..min(x_diff, y_diff) {
+                    if x_diff % i == 0 && y_diff % i == 0 {
+                        x_diff /= i;
+                        y_diff /= i;
+                    }
+                }
+
+                antinode_locations.insert(*a);
+                let mut multiplier = 1;
+                let mut stop_adding = false;
+                let mut stop_subtracting = false;
+                loop {
+                    if !stop_adding {
+                        let antinode = Coordinate { x: a.x + (multiplier * x_diff), y: a.y + (multiplier * y_diff) };
+
+                        if antinode.x >= 0 && antinode.x < map_height && antinode.y >= 0 && antinode.y < map_width {
+                            antinode_locations.insert(antinode);
+                        }
+                        else {
+                            stop_adding = true;
+                        }
+                    }
+
+                    if !stop_subtracting {
+                        let antinode = Coordinate { x: a.x - (multiplier * x_diff), y: a.y - (multiplier * y_diff) };
+
+                        if antinode.x >= 0 && antinode.x < map_height && antinode.y >= 0 && antinode.y < map_width {
+                            antinode_locations.insert(antinode);
+                        }
+                        else {
+                            stop_subtracting = true;
+                        }
+                    }
+
+                    if stop_adding && stop_subtracting {
+                        break;
+                    }
+
+                    multiplier += 1;
+                }
+            }
+        }
+    }
+
+    Some(antinode_locations.len() as u32)
 }
 
 #[cfg(test)]
